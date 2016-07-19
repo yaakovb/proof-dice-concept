77716b761be86365f7bb3871bad9b3bb563eb882
yaakov <yaakob56@gmail.com>
2016-07-19 23:08:10 +0300

    add factory

---
+++ proof dice concept/Factory.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace proof_dice_concept
+{
+    enum DiceTypes { TypeA, TypeB, TypeC, TypeD, TypeE }
+    class Factory
+    {
+        public BasicDiceSide CreateDiceSide(int val, DiceTypes dieType)
+        {
+            BasicDiceSide retVal;
+            switch (dieType)
+            {
+                case DiceTypes.TypeA:
+                    retVal = new TypeADiceSide(val);
+                    break;
+                case DiceTypes.TypeB:
+                    retVal = new TypeBDiceSide(val);
+                    break;
+                case DiceTypes.TypeC:
+                    retVal = new TypeCDiceSide(val);
+                    break;
+                case DiceTypes.TypeD:
+                    retVal = new TypeDDiceSide(val);
+                    break;
+                case DiceTypes.TypeE:
+                    retVal = new TypeEDiceSide(val);
+                    break;
+                default:
+                    retVal = null;
+                    break;
+            }
+            return retVal;
+        }
+    }
+}
