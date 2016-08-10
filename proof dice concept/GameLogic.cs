cc21618fc4279840d9ad9e7394eda2c8e24dc78d
yaakov <yaakob56@gmail.com>
2016-08-10 20:57:27 +0300

    added some game logic and round data
    first time commit at normal hour

--- proof dice concept/GameLogic.cs
+++ proof dice concept/GameLogic.cs
@@ -8,6 +8,36 @@ namespace proof_dice_concept
 {
     class GameLogic
     {
+        private Dictionary<DiceTypes, DiceTypes> refAnthema;
+        private static GameLogic instance;
+        public static GameLogic Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    instance = new GameLogic();
+                }
+                return instance;
+            }
+        }
+
+
+        public GameLogic()
+        {
+            refAnthema = new Dictionary<DiceTypes, DiceTypes>();
+            refAnthema[DiceTypes.TypeA] = DiceTypes.TypeC;
+            refAnthema[DiceTypes.TypeB] = DiceTypes.TypeD;
+            refAnthema[DiceTypes.TypeC] = DiceTypes.TypeE;
+            refAnthema[DiceTypes.TypeD] = DiceTypes.TypeA;
+            refAnthema[DiceTypes.TypeE] = DiceTypes.TypeB;
+        }
+
+        public bool IsDiceAnthemaToOtherDice(DiceTypes dice, DiceTypes otherDice)
+        {
+            return refAnthema[dice] == otherDice;
+        }
+
         public int[] GetSpin(int diceType)
         {
             int[] result = new int[2] { 0, 0 };
@@ -18,5 +48,7 @@ namespace proof_dice_concept
             result[1] = roll.M_val;
             return result;
         }
+
+        
     }
 }
---
+++ proof dice concept/Round.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace proof_dice_concept
+{
+    class Round
+    {
+        private Dice[] playerDice;
+        private Dice[] enemyDice;
+
+        public Round()
+        {
+            playerDice = new Dice[2] { null, null };
+            enemyDice = new Dice[2] { null, null };
+        }
+
+        private void SetDice(Dice[] targetPlayerDiceArray, int idx, DiceTypes diceType)
+        {
+            Dice d = new Dice();
+            d.Init(diceType);
+            targetPlayerDiceArray[idx] = d;
+        }
+
+        public void SetDice(bool isPlayerDice, int idx, int diceTypeVal)
+        {
+            DiceTypes diceType = (DiceTypes)diceTypeVal;
+            if (isPlayerDice)
+                SetDice(playerDice, idx, diceType);
+            else
+                SetDice(enemyDice, idx, diceType);
+        }
+
+        private BasicDiceSide playerSpin1;
+        public BasicDiceSide PlayerSpin1
+        {
+            get { return playerSpin1; }
+        }
+
+        private BasicDiceSide playerSpin2;
+        public BasicDiceSide PlayerSpin2
+        {
+            get { return playerSpin2; }
+        }
+
+        private BasicDiceSide enemySpin1;
+        public BasicDiceSide EnemySpin1
+        {
+            get { return enemySpin1; }
+        }
+
+        private BasicDiceSide enemySpin2;
+        public BasicDiceSide EnemySpin2
+        {
+            get { return enemySpin2; }
+        }
+
+        public bool IsStraightForcedOutCome()
+        {
+            bool val1 = GameLogic.Instance.IsDiceAnthemaToOtherDice(PlayerSpin1.M_type, EnemySpin1.M_type);
+            bool val2 = GameLogic.Instance.IsDiceAnthemaToOtherDice(PlayerSpin2.M_type, EnemySpin2.M_type);
+            return val1 || val2;
+        }
+
+        public bool IsCrossedForcedOutCome()
+        {
+            bool val1 = GameLogic.Instance.IsDiceAnthemaToOtherDice(PlayerSpin1.M_type, EnemySpin2.M_type);
+            bool val2 = GameLogic.Instance.IsDiceAnthemaToOtherDice(PlayerSpin2.M_type, EnemySpin1.M_type);
+            return val1 || val2;
+        }
+
+        public void Spin()
+        {
+            playerSpin1 = playerDice[0].DiceRoll;
+            playerSpin2 = playerDice[1].DiceRoll;
+            enemySpin1 = enemyDice[0].DiceRoll;
+            enemySpin2 = enemyDice[1].DiceRoll;
+        }
+    }
+}
